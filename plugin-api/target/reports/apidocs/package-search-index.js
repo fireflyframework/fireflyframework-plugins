67ddf90be8723da9aa78004e95dd5a47c62f6a1b
Carlos Sánchez <casc84ab@gmail.com>
2026-04-27 17:39:15 +0200

    release: bump version to 26.04.03

---
+++ plugin-api/target/reports/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.fireflyframework.plugin.annotation"},{"l":"org.fireflyframework.plugin.api"},{"l":"org.fireflyframework.plugin.model"}];updateSearchResults();
---
+++ plugin-core/target/reports/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.fireflyframework.plugin.core"}];updateSearchResults();
